6d4a74e103dceb4ba601c41fcf06a1edc38f0f90
WordlessEcho <wordless@echo.moe>
2023-07-30 23:56:57 +0800

    Allow null of password in UserDto

--- src/main/java/moe/echo/bramblingnote/user/UserDto.java
+++ src/main/java/moe/echo/bramblingnote/user/UserDto.java
@@ -24,7 +24,6 @@ public class UserDto implements Serializable {
     private String name;
 
     @JsonView(View.EditOnly.class)
-    @NotBlank
     private String password;
 
     @JsonView(View.Internal.class)
